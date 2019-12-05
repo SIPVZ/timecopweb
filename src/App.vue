662fd934ebe89e3a12fe5a5ca82eec3dd33000b2
s.iglesias.perez@bbva.com <s.iglesias.perez@bbva.com>
2019-12-05 01:49:43 +0100

    ver 2

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<v-app :dark="dark">
+  <v-content>
+    <router-view @toggleTheme="dark = $event"></router-view>
+  </v-content>
+</v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'App',
+  data: () => ({
+    dark: false
+  })
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  svg {
+    user-select: none;
+  }
 </style>
+
